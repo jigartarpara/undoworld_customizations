e7f43f4ff2599e74bf2e84d1f0e6689722b46bd3
Jigar Tarpara <jigartarpara@khatavahi.in>
2024-10-16 16:06:10 +0530

    API development

--- undoworld_customizations/hooks.py
+++ undoworld_customizations/hooks.py
@@ -31,7 +31,7 @@ app_license = "MIT"
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
+after_migrate = "undoworld_customizations.custom_field.setup_custom_fields"
 # Home Pages
 # ----------
 
