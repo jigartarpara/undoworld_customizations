9ababd00e2cdb34ee20087dc8c78760e50c72253
Jigar Tarpara <jigartarpara@khatavahi.in>
2024-11-13 09:15:15 +0530

    OMS Part

--- undoworld_customizations/apis/sales_order_update.py
+++ undoworld_customizations/apis/sales_order_update.py
@@ -4,8 +4,11 @@ import requests
 
 def on_update(doc, method):
     webhook_url = frappe.db.get_single_value("OMS Setup", "order_status_update_webhook")
-    if doc.has_value_changed("so_status"):
+    if webhook_url and doc.has_value_changed("so_status"):
         data = {
             "status": doc.so_status
         }
-        response = requests.post(url=webhook_url, data=data)
+        try:
+            response = requests.post(url=webhook_url, data=data)
+        except:
+            pass
