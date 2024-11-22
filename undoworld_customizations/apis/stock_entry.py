28991211633f664e15d9bde630685324ba47dfa1
Jigar Tarpara <jigartarpara@khatavahi.in>
2024-11-22 13:11:38 +0530

    Notification Changes

--- undoworld_customizations/apis/stock_entry.py
+++ undoworld_customizations/apis/stock_entry.py
@@ -7,7 +7,10 @@ def on_submit(doc, method):
                 for serial_no in row.serial_no.split("\n"):
                     support_ticket = frappe.db.get_value("Support Ticket", {"serial_number":  serial_no},"name")
                     if support_ticket:
-                        frappe.db.set_value("Support Ticket",support_ticket,  "status", "Device Received")
+                        st = frappe.get_doc("Support Ticket",support_ticket)
+                        st.status = "Device Received"
+                        st.ignore_permissions = True
+                        st.save()
     
     if doc.stock_entry_type == "Courier Out":
         for row in doc.items:
@@ -15,4 +18,7 @@ def on_submit(doc, method):
                 for serial_no in row.serial_no.split("\n"):
                     support_ticket = frappe.db.get_value("Support Ticket", {"serial_number":  serial_no},"name")
                     if support_ticket:
-                        frappe.db.set_value("Support Ticket",support_ticket,  "status", "Device Out for Delivery")
+                        st = frappe.get_doc("Support Ticket",support_ticket)
+                        st.status = "Device Out for Delivery"
+                        st.ignore_permissions = True
+                        st.save()
